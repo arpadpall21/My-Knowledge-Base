7fe90686c5ebe1cbe5e43ada24b789c66347a6b0
arpadpall21 <apall@hearsaycorp.com>
2023-08-05 13:30:01 +0200

    2023-08-05

---
+++ My Site/NGINX/TEST/expressSrv/srvExpress.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const args = process.argv;
+let port = 3000;
+
+for (let i = 0; args.length > i; i++) {
+  if (args[i] === '-p' && args[i + 1]) {
+    port = Number.parseInt(args[i + 1]);
+  }
+}
+
+let requestCounter = 0;
+app.use('/', (req, res) => {
+  res.send(`Server ${port} says: Hello World!`)
+  
+  requestCounter += 1;
+  console.log(requestCounter);
+})
+
+app.listen(port, () => console.log(`Express is listening on localhost:${port}`));
